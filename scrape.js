a8c66fc96bf7f409c705a85590538e394d4a7bbc
Jack Danger Canty <gitcommit@6brand.com>
2009-02-21 09:58:46 -0800

    basic twitter scrape written in javascript
    This is really ugly.  I shouldn't have to run Ruby.Hash.send("[]")
    just to create a new hash object.  I wonder if there's some way to
    just mark up a standard js object to have it interpreted as a Ruby
    hash.
    
    Back to reading the Johnson source code I guess.

---
+++ scrape.js
@@ -0,0 +1,18 @@
+Ruby.require('open-uri')
+
+Scrape = {
+  uri: "twitter.com/statuses/replies/",
+  username: "binarial",
+  password: Ruby.ENV['TWITTER_PASSWORD'],
+
+  retrieve: function(){
+    var url = 'http://'+this.uri+this.username+'.json';
+    var credentials =
+        Ruby.Hash.send("[]",
+                       Johnson.symbolize('http_basic_authentication'),
+                       Ruby.Array.send("[]", this.username, this.password)
+                      )
+    return Ruby.open(url, credentials).read()
+  },
+}
+  
