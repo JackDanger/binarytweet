7e36323086d065412eb4bb978d4d6b36b589c2b7
Jack Danger Canty <gitcommit@6brand.com>
2009-02-25 14:44:53 -0800

    web requires 'init.js'

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env jack
 
+Johnson.require('init')
+
 var render = function(template){
   Johnson.require("johnson/template")
   return Johnson.templatize(Ruby.File.read("views/"+template+".ejs"))
