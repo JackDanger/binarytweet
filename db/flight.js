3a9271e5ad053947ab12a6dab70067c1bff40175
Jack Danger Canty <gitcommit@6brand.com>
2009-02-25 22:37:14 -0800

    turned YAML db config into JSON db config

--- db/flight.js
+++ db/flight.js
@@ -1,89 +1,117 @@
-// Flight is an ultra-lightweight SQL ORM in javscript
-var Flight = (function(){
+// Flee the browser with an ultra-lightweight SQL ORM in Javscript
 
-  Ruby.require('rubygems')
-  Ruby.require('sqlite3')
+Johnson.require('johnson/browser')
+Johnson.require('johnson/browser/jquery')
 
-  var db_file = ('test' == Johnson.environment) ? 'sqlite.test.db' : 'sqlite.web.db'
+// Usually Flight is defined earlier on to give it configuration
+var Flight = Flight || {}
 
-  var connect = function(fn){
-    var db = Ruby.SQLite3.Database.new(db_file)
-    result = fn(db)
-    db.close()
-    return result;
-  }
+$.extend(Flight,
+  function(){
 
-  var fields = function(model){
-    if(!this.cachedFields) this.cachedFields = {}
-    if(this.cachedFields[model])
-      return this.cachedFields[model]
+    var config = (function(){
 
-    var schema = connect(function(db){ return db.table_info(model) });
-    var fields = []
-    for (var row in schema) fields.push(row["name"])
+      if(!Johnson.environment) Johnson.environment = 'production'
 
-    this.cachedFields[model] = fields
-    return fields
-  }
+      // The Flight.configuration must be either a javascript
+      // object or the path to a file containing one
+      // Format: {'development': {'adapter': 'sqlite3', 'database': 'my_db.sqlite'}}
 
-  var execute = function(sql){
-    Ruby.puts("SQL: \""+sql+"\"")
-    return connect(function(db){
-      return db.execute(sql)
-    })
-  }
+      if(!Flight.configuration)
+        throw("Flight.configuration is not defined")
 
-  var keys = function(obj){
-    var arr = []; for(var k in obj){arr.push(k)}; return arr;
-  }
+      var json_config = Flight.configuration && Flight.configuration.match(/\{|\}/) ?
+                          Flight.configuration : eval(Ruby.File.read(Flight.configuration))
+      return {
+        database: function(){return json_config[Johnson.environment]['database']},
+        adapter:  function(){return json_config[Johnson.environment]['adapter']}
+      }
+    })()
 
-  var values = function(obj){
-    var arr = []; for(var k in obj){arr.push(obj[k])}; return arr;
-  }
-
-  var attributesToConditions = function(attributes){
-    var conditions = []
-    var where = ''
-    for(var key in attributes){
-      where = ' WHERE '
-      conditions.push("`"+key+"` = \""+attributes[key]+"\"")
+    var connect = function(fn){
+      if('sqlite3' == config.adapter()) return connect_sqlite3(fn);
     }
-    return where+conditions.join(" AND ")
-  }
 
-  var records = function(model, sql){
-    ret = []
-    for(var row in execute(sql))
-      ret.push(instantiateRecord(model, row))
-    return ret;
-  }
-
-  var instantiateRecord = function(model, row){
-    var record = {}
-    for(var i=0; i<row.length(); i++)
-      record[fields(model)[i]] = row[i]
-
-    return record
-  }
-
-  // create the database file if it doesn't exist
-  if(!Ruby.File.send("exists?", db_file)){
-    execute(Ruby.File.read('schema.sql'))
-  }
-
-  // return a public object
-  return {
-
-    create: function(model, attributes){
-      return execute("INSERT INTO `"+model+"` "
-                     +" (`"+keys(attributes).join('`, `')+"`) "
-                     +" VALUES (\""+values(attributes).join('", "')+"\")")
-    },
-
-    find: function(model, attributes){
-      return records(model, "SELECT * FROM `"+model+"` "+attributesToConditions(attributes)+" LIMIT 1")
+    var connect_sqlite3 = function(fn){
+      Ruby.require('rubygems')
+      Ruby.require('sqlite3')
+      var db = Ruby.SQLite3.Database.new(config.database())
+      result = fn(db)
+      db.close()
+      return result;
     }
-  }
 
-})()
+    var fields = function(model){
+      if(!this.cachedFields) this.cachedFields = {}
+      if(this.cachedFields[model])
+        return this.cachedFields[model]
 
+      var schema = connect(function(db){ return db.table_info(model) });
+      var fields = []
+      for (var row in schema) fields.push(row["name"])
+
+      this.cachedFields[model] = fields
+      return fields
+    }
+
+    var execute = function(sql){
+      Ruby.puts("SQL: \""+sql+"\"")
+      return connect(function(db){
+        return db.execute(sql)
+      })
+    }
+
+    var keys = function(obj){
+      var arr = []; for(var k in obj){arr.push(k)}; return arr;
+    }
+
+    var values = function(obj){
+      var arr = []; for(var k in obj){arr.push(obj[k])}; return arr;
+    }
+
+    var attributesToConditions = function(attributes){
+      var conditions = []
+      var where = ''
+      for(var key in attributes){
+        where = ' WHERE '
+        conditions.push("`"+key+"` = \""+attributes[key]+"\"")
+      }
+      return where+conditions.join(" AND ")
+    }
+
+    var records = function(model, sql){
+      ret = []
+      for(var row in execute(sql))
+        ret.push(instantiateRecord(model, row))
+      return ret;
+    }
+
+    var instantiateRecord = function(model, row){
+      var record = {}
+      for(var i=0; i<row.length(); i++)
+        record[fields(model)[i]] = row[i]
+
+      return record
+    }
+
+    // create the database file if it doesn't exist
+    if('sqlite3' == config.adapter())
+      if(!Ruby.File.send("exists?", config.database()))
+        execute(Ruby.File.read('schema.sql'))
+
+    // return a public object
+    return {
+
+      create: function(model, attributes){
+        return execute("INSERT INTO `"+model+"` "
+                       +" (`"+keys(attributes).join('`, `')+"`) "
+                       +" VALUES (\""+values(attributes).join('", "')+"\")")
+      },
+
+      find: function(model, attributes){
+        return records(model, "SELECT * FROM `"+model+"` "+attributesToConditions(attributes)+" LIMIT 1")
+      }
+    }
+
+  }()
+)
