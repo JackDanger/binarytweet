88b4df6d98123980239060872a3f31091e297315
Jack Danger Canty <gitcommit@6brand.com>
2009-02-25 23:41:56 -0800

    having to execute sql line-by-line :/

--- init.js
+++ init.js
@@ -6,10 +6,11 @@ Johnson.require('tojson')
 Johnson.require('db/flight')
 Flight.configuration = 'db/config.json'
 // create the database file if it doesn't exist
-if('sqlite3' == Flight.config.adapter()
-   &&  !Ruby.File.send(
-         "exists?",
-         Flight.config.database()
-       ))
-  Flight.execute(Ruby.File.read('db/schema.sql'))
+if('sqlite3' == Flight.config.adapter())
+  if(!Ruby.File.send("exists?",
+                     Flight.config.database()
+                    )){
+  for each(line in Ruby.File.read('db/schema.sql').split(";"))
+    Flight.execute(line)
+}
 
