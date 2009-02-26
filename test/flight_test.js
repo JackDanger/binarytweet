70d5c5ab67d7105f71b165bf4e2398de912f216c
Jack Danger Canty <gitcommit@6brand.com>
2009-02-25 22:37:36 -0800

    fixing test require

--- test/flight_test.js
+++ test/flight_test.js
@@ -2,7 +2,7 @@ Johnson.require("test_helper")
 
 jspec.describe("Flight", function() {
   it("loads Flight", function() {
-    Johnson.require("db/flight");
+    Johnson.require("init");
     expect(Flight.create).to("have_constructor", Function);
   });
 });
