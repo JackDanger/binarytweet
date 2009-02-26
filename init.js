762c2499fa55b613d17630d0b8261366176cd3af
Jack Danger Canty <gitcommit@6brand.com>
2009-02-25 23:06:35 -0800

    not configuring Flight until after it's required

--- init.js
+++ init.js
@@ -3,5 +3,13 @@ Johnson.require('johnson/browser')
 Johnson.require('johnson/browser/jquery')
 Johnson.require('tojson')
 
-Flight = {configuration: 'db/config.json'}
 Johnson.require('db/flight')
+Flight.configuration = 'db/config.json'
+// create the database file if it doesn't exist
+if('sqlite3' == Flight.config.adapter()
+   &&  !Ruby.File.send(
+         "exists?",
+         Flight.config.database()
+       ))
+  Flight.execute(Ruby.File.read('db/schema.sql'))
+
