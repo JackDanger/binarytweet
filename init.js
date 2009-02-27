2bc4c5ff4e10204665a3e2e5ce471e05c9544315
Jack Danger Canty <gitcommit@6brand.com>
2009-02-26 22:23:13 -0800

    Automatically adding Johnson root to LOAD_PATH

--- init.js
+++ init.js
@@ -1,10 +1,20 @@
 
+// Add the Johnson library root to LOAD_PATH so
+// we can include fun parts when we want to
+for (var dir in Ruby["$LOAD_PATH"])
+  if(/johnson-[\d-\.]+\/lib$/.test(dir)) break
+dir = dir.split('/'); dir.pop();
+Ruby["$LOAD_PATH"].unshift(dir.join("/"))
+Ruby["$LOAD_PATH"].unshift('.')
+
 Johnson.require('johnson/browser')
 Johnson.require('johnson/browser/jquery')
 Johnson.require('tojson')
 
+
 Johnson.require('db/flight')
 Flight.configuration = 'db/config.json'
+
 // create the database file if it doesn't exist
 if('sqlite3' == Flight.config.adapter())
   if(!Ruby.File.send("exists?",
@@ -13,4 +23,3 @@ if('sqlite3' == Flight.config.adapter())
   for each(line in Ruby.File.read('db/schema.sql').split(";"))
     Flight.execute(line)
 }
-
