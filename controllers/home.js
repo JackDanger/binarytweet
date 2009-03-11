bca7640e7858bd9a986402f293fa50162cec48a1
Jack Danger Canty <gitcommit@6brand.com>
2009-03-10 19:55:36 -0700

    moving low-priority route into a controller

--- controllers/home.js
+++ controllers/home.js
@@ -4,4 +4,17 @@ Jack.Action(/^\/$/,function() {
   var tweets = Flight.find('tweets', {}, 10)
 
   return render('home').call({tweets: tweets})
-});
+})
+
+
+// low-priority route for serving public files
+Jack.Action(/.*/, function(env, params) {
+  var filename = Ruby.File.dirname(__FILE__)+'/public'+env['PATH_INFO']
+  return Ruby.File.send("exist?", filename) ?
+    [
+      200,
+      {'Content-Type': 'image/jpeg'},
+      Ruby.File.read(filename)
+    ] :
+    Jack.not_found()
+})
--- web.js
+++ web.js
@@ -7,16 +7,4 @@ var render = function(template){
   return Johnson.templatize(Ruby.File.read("views/"+template+".ejs"))
 };
 
-// low-priority route for serving public files
-Jack.Action(/.*/, function(env, params) {
-  var filename = Ruby.File.dirname(__FILE__)+'/public'+env['PATH_INFO']
-  return Ruby.File.send("exist?", filename) ?
-    [
-      200,
-      {'Content-Type': 'image/jpeg'},
-      Ruby.File.read(filename)
-    ] :
-    Jack.not_found()
-})
-
 Jack.up()
