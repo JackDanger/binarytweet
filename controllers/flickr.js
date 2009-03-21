58d8ca5aad0f4f9f20799600884f8fe5e8de2a3a
Jack Danger Canty <gitcommit@6brand.com>
2009-03-20 20:00:04 -0700

    adding flickr photos.

--- controllers/flickr.js
+++ controllers/flickr.js
@@ -4,16 +4,15 @@ Jack.Action(/^\/flickr\.js/, function(){
 
     var API_KEY = '3a26f36ac015fa09111911faeaf6eedc'
 
-    randomizeTags = function(tagString){
-      var tags = tagString.split(",")
-      var i = tags.length;
+    randomizeArray = function(arr){
+      var i = arr.length;
       while(--i) {
        var r = Math.floor( Math.random() * ( i + 1 ) );
-       var tmp = tags[i]
-       tags[i] = tags[r]
-       tags[r] = tmp
+       var tmp = arr[i]
+       arr[i] = arr[r]
+       arr[r] = tmp
       }
-      return tags
+      return arr
     }
 
     loadPhotos = function(tags, destination){
@@ -29,22 +28,23 @@ Jack.Action(/^\/flickr\.js/, function(){
           "http://api.flickr.com/services/rest/?"+
           "method=flickr.photos.search"+
           "&api_key="+API_KEY+
-          "&tags="+randomizeTags(tags)+
+          "&tags="+randomizeArray(tags.split(",")).slice(10).join(",")+
           "&format=json&jsoncallback=?",
-        function (data) {
-          $.each(data.photos.photo, function (_, photo){
-            $(destination.shift())
-              .append(
+          function (data) {
+            var photos = randomizeArray(data.photos.photo)
+            destination.each(function(){
+              var photo = photos.shift()
+              $(this).append(
                 $("<img/>")
                   .attr("src", photo_src(photo, 's'))
               )
-          });
-        }
+            })
+          }
       );
     }
     $(function(){
-      loadPhotos('no,zero,bad,awful,terrible,rancid,hate,suck,blast,stupid,dead,broken,trashed', $('.zero .photo'))
-      loadPhotos('yes,one,good,great,excellent,sweet,love,cool,beauty,smart,lively,working,clean', $('.one .photo'))
+      loadPhotos('no,zero,bad,awful,terrible,rancid,hate,suck,blast,stupid,dead,broken,trashed', $('.zero'))
+      loadPhotos('yes,one,good,great,excellent,sweet,love,cool,beauty,smart,lively,working,clean', $('.one'))
     })
   ]]></r>;
 })
